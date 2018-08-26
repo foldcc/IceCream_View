1ef32caf96d1afe0eecd5e938e1538401158f648
Foldcc <1813547935@qq.com>
2018-08-26 23:32:53 +0800

    增加用于模块化页面的抽象类

---
+++ Assets/IcecreamView/Scripts/abstract/GameViewAbstractModule.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IcecreamView {
+    /// <summary>
+    /// gameView模板类
+    /// </summary>
+    [RequireComponent(typeof(GameViewModuleConnector)) , AddComponentMenu("IcecreamModule/")]
+    public class GameViewAbstractModule : MonoBehaviour, GameViewInterface
+    {
+        [Header("执行优先级")]
+        public int prioritylevel = 1;
+
+        public virtual void View_Destory()
+        {
+
+        }
+
+        public virtual void View_Disable()
+        {
+
+        }
+
+        public virtual void View_Enable()
+        {
+
+        }
+
+        public virtual void View_Init()
+        {
+
+        }
+    }
+}
+
