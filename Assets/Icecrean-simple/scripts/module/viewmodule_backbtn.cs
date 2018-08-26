2a430a74622b8caacc9a979e64be8ac015ed08ba
Foldcc <1813547935@qq.com>
2018-08-27 00:30:00 +0800

    增加Simple实例

---
+++ Assets/Icecrean-simple/scripts/module/viewmodule_backbtn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IcecreamView;
+using UnityEngine.UI;
+
+public class viewmodule_backbtn : GameViewAbstractModule {
+
+    public Button backBtn;
+
+    public override void DestoryView()
+    {
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public override void OnOpenView()
+    {
+    }
+
+    public override void OnCloseView()
+    {
+    }
+
+    public override void OnInitView()
+    {
+    }
+
+}
---
+++ Assets/Icecrean-simple/scripts/module/viewmodule_toview.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using IcecreamView;
+
+public class viewmodule_toview : GameViewAbstractModule
+{
+    public Button toview;
+
+    public string viewTable;
+
+    public override void OnInitView()
+    {
+
+    }
+
+}
