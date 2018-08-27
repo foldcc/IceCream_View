507c4eb7ec213bacad11be1ea29be195d4fcec0c
ZHBSFoldcc <foldcc@zhbs.com>
2018-08-27 11:40:10 +0800

    完善simple示例演示

---
+++ Assets/Icecrean-simple/scripts/ViewController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IcecreamView;
+
+public class ViewController : MonoBehaviour {
+
+    public GameViewConfig viewConfig;
+
+    public Transform Canvasteansform;
+	
+    private GameViewManager viewManager;
+
+
+    private void Start()
+    {
+        viewManager = new GameViewManager();
+        viewManager.Init(viewConfig , Canvasteansform);
+    }
+}
