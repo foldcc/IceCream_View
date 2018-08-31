03a4d23205ce73562fefb137d2e013360278d802
ZHBSFoldcc <1813547935@zhbs.com>
2018-08-31 19:06:50 +0800

    增加空白页面 以及 空白模块

---
+++ Assets/IcecreamView/Scripts/component/GameViewNonModule.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace IcecreamView {
+
+    public class GameViewNonModule : GameViewAbstractModule
+    {
+
+        public bool isDebugLog = false;
+
+        public override void OnInitView()
+        {
+            base.OnInitView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonModule OnInit");
+        }
+
+        public override void OnCloseView()
+        {
+            base.OnCloseView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonModule OnClose");
+        }
+
+        public override void OnOpenView()
+        {
+            base.OnOpenView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonModule OnOpen");
+        }
+    }
+
+}
---
+++ Assets/IcecreamView/Scripts/component/GameViewNonView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IcecreamView {
+    public class GameViewNonView : GameViewAbstract
+    {
+        public bool isDebugLog = false;
+        public override void OnInitView()
+        {
+            base.OnInitView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonView OnInit");
+        }
+
+        public override void OnCloseView()
+        {
+            base.OnCloseView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonView OnClose");
+        }
+
+        public override void OnOpenView()
+        {
+            base.OnOpenView();
+            if (!isDebugLog) return;
+            Debug.Log("GameViewNonView OnOpen");
+        }
+    }
+}
+
+
