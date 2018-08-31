63560902d91a1883873473d28e796855ce63624e
ZHBSFoldcc <1813547935@zhbs.com>
2018-08-31 19:06:25 +0800

    现在每一个页面只能设置一个视图组件

--- Assets/IcecreamView/Scripts/abstract/GameViewAbstract.cs
+++ Assets/IcecreamView/Scripts/abstract/GameViewAbstract.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace IcecreamView {
-
+    [DisallowMultipleComponent]
     public abstract class GameViewAbstract : MonoBehaviour , GameViewInterface
     {
         /// <summary>
