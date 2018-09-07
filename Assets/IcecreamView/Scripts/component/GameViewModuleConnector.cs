7cb15c07d6d6655a87ff169a11b49564bffcaa4c
ZHBSFoldcc <1813547935@zhbs.com>
2018-09-07 12:12:16 +0800

    优化模块连接器，每个对象限制一个，新增await操作

--- Assets/IcecreamView/Scripts/component/GameViewModuleConnector.cs
+++ Assets/IcecreamView/Scripts/component/GameViewModuleConnector.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,53 +8,114 @@ namespace IcecreamView
     /// <summary>
     /// 使用模板组合View模式的必要组件
     /// </summary>
+    [DisallowMultipleComponent]
     public sealed class GameViewModuleConnector : GameViewAbstract
     {
-        private List<GameViewAbstractModule> gameViewAbstractModules = new List<GameViewAbstractModule>();
+        private List<GameViewAbstractModule> gameViewAbstractModules = null;
+
+        private RunType awaitType;
+
+        private int awaitCount = 0;
+
+        private bool isAwait = false;
 
         public override void OnOpenView()
         {
-            foreach (var item in gameViewAbstractModules)
+            awaitType = RunType.OnOpen;
+            while (awaitCount < gameViewAbstractModules.Count)
             {
-                item.OnOpenView();
+                gameViewAbstractModules[awaitCount].OnOpenView();
+                awaitCount++;
+                if (isAwait) {
+                    return;
+                }
             }
+            awaitCount = 0;
             base.OnOpenView();
         }
 
         public override void OnCloseView()
         {
-            foreach (var item in gameViewAbstractModules)
+            awaitType = RunType.OnClose;
+            while (awaitCount < gameViewAbstractModules.Count)
             {
-                item.OnCloseView();
+                gameViewAbstractModules[awaitCount].OnCloseView();
+                awaitCount++;
+                if (isAwait)
+                {
+                    return;
+                }
             }
+            awaitCount = 0;
         }
 
         public override void OnInitView()
         {
-            GetComponents(gameViewAbstractModules);
-            gameViewAbstractModules.Sort((GameViewAbstractModule a , GameViewAbstractModule b) => {
-                if (a.prioritylevel < b.prioritylevel) {
-                    return -1;
-                } else if(a.prioritylevel > b.prioritylevel)
-                {
-                    return 1;
-                }
-                return 0;
-            });
-
-            gameViewAbstractModules.ForEach(m => {
-                m.viewConnector = this;
-            });
-
+            awaitType = RunType.OnInit;
             if (gameViewAbstractModules == null)
             {
-                gameViewAbstractModules = new List<GameViewAbstractModule>(); 
+                gameViewAbstractModules = new List<GameViewAbstractModule>();
+                //获取当前对象上的所有View模块
+                GetComponents(gameViewAbstractModules);
+                //重新根据优先级排序
+                gameViewAbstractModules.Sort((GameViewAbstractModule a, GameViewAbstractModule b) => {
+                    if (a.prioritylevel < b.prioritylevel)
+                    {
+                        return -1;
+                    }
+                    else if (a.prioritylevel > b.prioritylevel)
+                    {
+                        return 1;
+                    }
+                    return 0;
+                });
+                //初始化所有模块
+                gameViewAbstractModules.ForEach(m => {
+                    m.viewConnector = this;
+                });
             }
-
-            foreach (var item in gameViewAbstractModules)
+            while (awaitCount < gameViewAbstractModules.Count)
             {
-                item.OnInitView();
+                gameViewAbstractModules[awaitCount].OnInitView();
+                awaitCount++;
+                if (isAwait)
+                {
+                    return;
+                }
             }
+            awaitCount = 0;
+        }
+
+        /// <summary>
+        /// 停止该页面正在执行的生命周期 ,停止的范围仅限(OnInit、OnOpen、OnClose)
+        /// 不支持多线程操作
+        /// </summary>
+        /// <returns>用于重新恢复中断的生命周期执行器</returns>
+        public Action Await() {
+            isAwait = true;
+            return Continue;
+        }
+
+        private void Continue() {
+            if (!isAwait) return;
+            isAwait = false;
+            switch (awaitType) {
+                case RunType.OnClose:
+                    OnCloseView();
+                    break;
+                case RunType.OnInit:
+                    OnCloseView();
+                    break;
+                case RunType.OnOpen:
+                    OnOpenView();
+                    break;
+            }
+        }
+
+        private enum RunType{
+            OnInit,
+            OnOpen,
+            OnClose
         }
     }
 }
