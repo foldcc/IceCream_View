b55a33ee453341d5ca293799a863f9d9abd8d888
ZHBSFoldcc <1813547935@zhbs.com>
2018-09-25 11:14:22 +0800

    增加注释

--- Assets/IcecreamView/Scripts/interface/GameViewInterface.cs
+++ Assets/IcecreamView/Scripts/interface/GameViewInterface.cs
@@ -1,13 +1,24 @@
 ﻿namespace IcecreamView {
-
+    /// <summary>
+    /// icecream页面接口
+    /// </summary>
     public interface GameViewInterface
     {
+        /// <summary>
+        /// 页面被初始化时触发
+        /// </summary>
         void OnInitView();
-
+        /// <summary>
+        /// 页面被打开时触发
+        /// </summary>
         void OnOpenView();
-
+        /// <summary>
+        /// 页面被关闭时触发
+        /// </summary>
         void OnCloseView();
-
+        /// <summary>
+        /// 关闭当前页面
+        /// </summary>
         void CloseView();
     }
 
