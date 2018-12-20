3c9147e10d14655e5e90e92c4f3e70c76577a16e
ZHBSFoldcc <1813547935@zhbs.com>
2018-12-20 20:47:19 +0800

    优化UI控制器的代码

--- Assets/IcecreamView/Scripts/manager/GameViewManager.cs
+++ Assets/IcecreamView/Scripts/manager/GameViewManager.cs
@@ -28,11 +28,183 @@ namespace IcecreamView
         {
             Init(gameViewConfig, parent);
         }
-
-        [Obsolete("不在提供运行时修改View配置表", true)]
-        public void SetConfig(GameViewConfig config)
+        
+        /// <summary>
+        /// 获取指定页面
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public T GetView<T>(string table) where T : GameViewAbstract
         {
-            Config = config;
+            if (ContainsKeyView(table))
+            {
+                foreach (var item in ViewDictionary)
+                {
+                    if (table.Equals(item.VIEWTABLE))
+                    {
+                        return (T)item;
+                    }
+                }
+            }
+            return default(T);
+        }
+
+        public T OpenView<T>(string table, bool isSinge = false) where T : GameViewAbstract
+        {
+            GameViewAbstract view = OpenView(table, isSinge);
+            if (view != null)
+            {
+                return view as T;
+            }
+            return null;
+        }
+
+        public T OpenView<T>(bool isSinge = false) where T : GameViewAbstract
+        {
+            string cname = typeof(T).ToString();
+            foreach (GameViewAbstract key in ViewDictionary)
+            {
+                if (key.GetType().Name.Equals(cname))
+                {
+                    return OpenView<T>(key.VIEWTABLE, isSinge);
+                }
+            }
+            Debug.LogError("GameViewManager : 打开view失败，未找到指定T --- " + cname);
+            return null;
+        }
+
+        /// <summary>
+        /// 构建一个View对象
+        /// </summary>
+        /// <param name="Table">ViewTable</param>
+        /// <returns></returns>
+        private GameViewAbstract CreateView(string Table)
+        {
+            GameViewAbstract gameViewAbstract = GameObject.Instantiate<GameViewAbstract>(ConfigViewDictionary[Table].View, UIparent);
+            gameViewAbstract.VIEWTABLE = Table;
+            if (ConfigViewDictionary[Table].isOnce)
+            {
+                gameViewAbstract.isOnce = ConfigViewDictionary[Table].isOnce;
+            }
+            else
+            {
+                gameViewAbstract.isOnce = ContainsKeyView(Table);
+            }
+            gameViewAbstract.SetViewManager(this);
+            gameViewAbstract.isOpen = false;
+            gameViewAbstract.OnInitView();
+            return gameViewAbstract;
+        }
+        /// <summary>
+        /// 打开指定页面
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="isSinge"></param>
+        /// <returns></returns>
+        public GameViewAbstract OpenView(string table, bool isSinge = false)
+        {
+            int viewCount = getViewIndex(table, isSinge);
+            if (viewCount != -1)
+            {
+                ViewDictionary[viewCount].isOpen = true;
+                ViewDictionary[viewCount].gameObject.SetActive(true);
+                ViewDictionary[viewCount].transform.SetAsLastSibling();
+                ViewDictionary[viewCount].OnOpenView();
+                return ViewDictionary[viewCount];
+            }
+            else if (ConfigViewDictionary.ContainsKey(table))
+            {
+                GameViewAbstract gameViewAbstract = CreateView(table);
+                gameViewAbstract.isOpen = true;
+                gameViewAbstract.gameObject.SetActive(true);
+                gameViewAbstract.transform.SetAsLastSibling();
+                gameViewAbstract.OnOpenView();
+                ViewDictionary.Add(gameViewAbstract);
+                return gameViewAbstract;
+            }
+            else
+            {
+
+                Debug.LogFormat("<color=yellow>{0}</color>" , "GameViewManager : 打开view失败，未找到指定table --- " + table);
+                return null;
+            }
+        }
+        /// <summary>
+        /// 关闭所有页面 并打开指定页面
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public GameViewAbstract OpenViewAndCloseOther(string table , bool isSinge)
+        {
+            if (table == null)
+            {
+                return null;
+            }
+
+            foreach (var item in ViewDictionary)
+            {
+                if (item.isOpen)
+                {
+                    item.CloseView();
+                }
+            }
+
+            GameViewAbstract view = OpenView(table , isSinge);
+            return view;
+        }
+
+        /// <summary>
+        /// 打开指定的ViewModuleConnector页面
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="isSinge"></param>
+        /// <returns></returns>
+        public GameViewModuleConnector OpenConnectorView(string table, bool isSinge = false)
+        {
+            return OpenView<GameViewModuleConnector>(table, isSinge);
+        }
+
+        /// <summary>
+        /// 判断指定table是否存在
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public bool ContainsKeyView(string table)
+        {
+            foreach (GameViewAbstract G in ViewDictionary)
+            {
+                if (table.Equals(G.VIEWTABLE))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 返回一个指定table的View对应下标
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public int getViewIndex(string table, bool isSinge = false, bool objectType = false)
+        {
+            for (int i = 0; i < ViewDictionary.Count; i++)
+            {
+
+                if (table.Equals(ViewDictionary[i].VIEWTABLE))
+                {
+                    if (isSinge)
+                    {
+                        return i;
+                    }
+                    else if (ViewDictionary[i].gameObject.activeSelf == objectType)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
         }
 
         private void Init(GameViewConfig gameViewConfig, Transform parent)
@@ -66,147 +238,6 @@ namespace IcecreamView
             {
                 OpenView(Config.DefaultViewTable);
             }
-        }
-
-        /// <summary>
-        /// 构建一个View对象
-        /// </summary>
-        /// <param name="Table">ViewTable</param>
-        /// <returns></returns>
-        private GameViewAbstract CreateView(string Table)
-        {
-            GameViewAbstract gameViewAbstract = GameObject.Instantiate<GameViewAbstract>(ConfigViewDictionary[Table].View, UIparent);
-            gameViewAbstract.VIEWTABLE = Table;
-            if (ConfigViewDictionary[Table].isOnce)
-            {
-                gameViewAbstract.isOnce = ConfigViewDictionary[Table].isOnce;
-            }
-            else {
-                gameViewAbstract.isOnce = ContainsKeyView(Table);
-            }
-            gameViewAbstract.SetViewManager(this);
-            gameViewAbstract.isOpen = false;
-            gameViewAbstract.OnInitView();
-            return gameViewAbstract;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="table"></param>
-        /// <returns></returns>
-        public bool ContainsKeyView(string table)
-        {
-            foreach (GameViewAbstract G in ViewDictionary)
-            {
-                if (table.Equals(G.VIEWTABLE))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// 返回一个指定table的View对应下标
-        /// </summary>
-        /// <param name="table"></param>
-        /// <returns></returns>
-        public int getViewIndex(string table, bool isSinge = false , bool objectType = false)
-        {
-            for (int i = 0; i < ViewDictionary.Count; i++)
-            {
-
-                if (table.Equals(ViewDictionary[i].VIEWTABLE))
-                {
-                    if (isSinge)
-                    {
-                        return i;
-                    }
-                    else if (ViewDictionary[i].gameObject.activeSelf == objectType)
-                    {
-                        return i;
-                    }
-                }
-            }
-            return -1;
-        }
-
-        public T OpenView<T>(string table, bool isSinge = false) where T : GameViewAbstract
-        {
-            GameViewAbstract view = OpenView(table, isSinge);
-            if (view != null)
-            {
-                return view as T;
-            }
-            return null;
-        }
-
-        public T OpenView<T>(bool isSinge = false) where T : GameViewAbstract
-        {
-            string cname = typeof(T).ToString();
-            foreach (GameViewAbstract key in ViewDictionary)
-            {
-                if (key.GetType().Name.Equals(cname))
-                {
-                    return OpenView<T>(key.VIEWTABLE, isSinge);
-                }
-            }
-            Debug.LogError("GameViewManager : 打开view失败，未找到指定T --- " + cname);
-            return null;
-        }
-
-        public GameViewAbstract OpenView(string table, bool isSinge = false)
-        {
-            int viewCount = getViewIndex(table, isSinge);
-            if (viewCount != -1)
-            {
-                ViewDictionary[viewCount].isOpen = true;
-                ViewDictionary[viewCount].gameObject.SetActive(true);
-                ViewDictionary[viewCount].transform.SetAsLastSibling();
-                ViewDictionary[viewCount].OnOpenView();
-                return ViewDictionary[viewCount];
-            }
-            else if (ConfigViewDictionary.ContainsKey(table))
-            {
-                GameViewAbstract gameViewAbstract = CreateView(table);
-                gameViewAbstract.isOpen = true;
-                gameViewAbstract.gameObject.SetActive(true);
-                gameViewAbstract.transform.SetAsLastSibling();
-                gameViewAbstract.OnOpenView();
-                ViewDictionary.Add(gameViewAbstract);
-                return gameViewAbstract;
-            }
-            else
-            {
-
-                Debug.LogFormat("<color=yellow>{0}</color>" , "GameViewManager : 打开view失败，未找到指定table --- " + table);
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 关闭所有页面 并打开指定页面
-        /// </summary>
-        /// <param name="table"></param>
-        /// <returns></returns>
-        public GameViewAbstract OpenViewAndCloseOther(string table , bool isSinge)
-        {
-            if (table == null)
-            {
-                return null;
-            }
-
-            foreach (var item in ViewDictionary)
-            {
-                if (item.isOpen)
-                {
-                    item.CloseView();
-                }
-            }
-
-            GameViewAbstract view = OpenView(table , isSinge);
-            return view;
         }
 
         /// <summary>
@@ -266,27 +297,6 @@ namespace IcecreamView
         }
 
         /// <summary>
-        /// 获取指定页面
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="table"></param>
-        /// <returns></returns>
-        public T GetView<T>(string table) where T : GameViewAbstract
-        {
-            if (ContainsKeyView(table))
-            {
-                foreach (var item in ViewDictionary)
-                {
-                    if (table.Equals(item.VIEWTABLE))
-                    {
-                        return (T)item;
-                    }
-                }
-            }
-            return default(T);
-        }
-
-        /// <summary>
         /// 销毁指定的view
         /// </summary>
         /// <param name="hash"></param>
@@ -303,5 +313,10 @@ namespace IcecreamView
             }
         }
 
+        [Obsolete("不在提供运行时修改View配置表", true)]
+        public void SetConfig(GameViewConfig config)
+        {
+            Config = config;
+        }
     }
 }
